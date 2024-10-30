cd404c237bea1db7d365058027758e05af006310
IskanderDev01 <githubturabaevi01@gmail.com>
2024-10-30 13:25:26 +0500

    as

--- src/pages/MainPage/ui/MainPage/AudioPlayer.tsx
+++ src/pages/MainPage/ui/MainPage/AudioPlayer.tsx
@@ -1,31 +1,31 @@
-import { faPlay, faStop } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faPlay, faStop } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState, useEffect } from 'react';
 import ReactPlayer from 'react-player';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import audioFile from '../../../../shared/assets/music.mp3'; // Убедитесь, что путь правильный
 
-const ControlsContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 15px;
-`;
+// const ControlsContainer = styled.div`
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     gap: 15px;
+// `;
 
-const Button = styled.button`
-    background-color: #9d8280;
-    color: white;
-    border: none;
-    border-radius: 50%;
-    width: 40px;
-    height: 40px;
-    cursor: pointer;
-    transition: background-color 0.3s;
+// const Button = styled.button`
+//     background-color: #9d8280;
+//     color: white;
+//     border: none;
+//     border-radius: 50%;
+//     width: 40px;
+//     height: 40px;
+//     cursor: pointer;
+//     transition: background-color 0.3s;
 
-    &:hover {
-        background-color: #7b6e6c;
-    }
-`;
+//     &:hover {
+//         background-color: #7b6e6c;
+//     }
+// `;
 
 const AudioPlayer = () => {
     const [playing, setPlaying] = useState(false);
@@ -34,17 +34,17 @@ const AudioPlayer = () => {
         setPlaying(true); // Начинаем воспроизведение при загрузке страницы
     }, []);
 
-    const togglePlay = () => {
-        setPlaying((prev) => !prev); // Переключаем состояние воспроизведения
-    };
+    // const togglePlay = () => {
+    //     setPlaying((prev) => !prev); // Переключаем состояние воспроизведения
+    // };
 
     return (
         <div className="bg-transparent fixed z-50 right-5 bottom-16 rounded-full">
-            <ControlsContainer>
+            {/* <ControlsContainer>
                 <Button onClick={togglePlay}>
                     <FontAwesomeIcon icon={playing ? faStop : faPlay} />
                 </Button>
-            </ControlsContainer>
+            </ControlsContainer> */}
             <ReactPlayer
                 url={audioFile}
                 playing={playing}
